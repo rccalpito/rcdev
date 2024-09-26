68a916f3200e0ba6dd3b5ea6d92d3b342d2701e0
rccalpito <rccalpito@gmail.com>
2024-09-26 11:49:12 -0700

    move all settings to explicit config files

---
+++ backend/config/database.py
@@ -0,0 +1,15 @@
+# Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+from backend.config.base import BASE_DIR
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
---
+++ backend/config/static.py
@@ -0,0 +1,6 @@
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.1/howto/static-files/
+
+STATIC_URL = "/static/"
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
