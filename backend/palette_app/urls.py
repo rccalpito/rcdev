fe3807715c59167136f4f001b4cf9a8032f8b0f3
rccalpito <rccalpito@gmail.com>
2024-10-02 13:37:46 -0700

    update urls.py palette app for url pattern

--- backend/palette_app/urls.py
+++ backend/palette_app/urls.py
@@ -3,4 +3,5 @@ from .views import generate_common_colors
 
 urlpatterns = [
     path('', generate_common_colors, name='generate_common_colors'),
+    path('color-palette/', generate_common_colors, name='color-palette-upload')
 ]
